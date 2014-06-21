25e27cb85f60afb731587845b279150c4c5c05f1
Wen-Tien Chang <ihower@gmail.com>
2014-06-21 22:28:31 +0800

    Update README

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,11 @@ class WelcomeController < ApplicationController
 
   def index
     render :json => {
-      :repository_url => "https://github.com/ihower/dcf",
       :politicians_url => politicians_url,
       :accounts_url => accounts_url,
       :entities_url => entities_url,
-      :codes_url => codes_url
+      :codes_url => codes_url,
+      :repository_url => "https://github.com/ihower/dcf"
     }
   end
 
