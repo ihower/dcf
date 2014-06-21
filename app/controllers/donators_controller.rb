db6dbd30a97182f2918494834d0b36bcaf2dde79
Wen-Tien Chang <ihower@gmail.com>
2014-06-21 17:34:31 +0800

    Add Donator identification

--- app/controllers/donators_controller.rb
+++ app/controllers/donators_controller.rb
@@ -10,6 +10,7 @@ class DonatorsController < ApplicationController
     render :json => donators.map{ |p|
       { :id => p.id,
         :name => p.name,
+        :identification => p.identification,
         :transactions_count => p.transactions_count,
         :income_amount => p.income_amount,
         :payout_amount => p.payout_amount,
