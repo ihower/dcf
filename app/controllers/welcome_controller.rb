266bffc3790f5b9be1579de2f5c57d217e53c4ab
Wen-Tien Chang <ihower@gmail.com>
2014-06-21 22:38:48 +0800

    Add two more root links

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,8 @@ class WelcomeController < ApplicationController
       :politicians_url => politicians_url,
       :accounts_url => accounts_url,
       :entities_url => entities_url,
+      :entities_by_income_url => entities_url( :order => "income_amount" ),
+      :entities_by_payout_url => entities_url( :order => "payout_amount" ),
       :codes_url => codes_url,
       :repository_url => "https://github.com/ihower/dcf"
     }
